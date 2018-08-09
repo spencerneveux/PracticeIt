4cc18cdbe6102385ac9a75765e1c7704a1b4ff7a
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-09 13:02:29 -0700

    Create writeSquares.java

---
+++ Chapter 12/writeSquares.java
@@ -0,0 +1,17 @@
+public void writeSquares(int n) {
+    if (n < 1) {throw new IllegalArgumentException();}
+    
+    if (n == 1) {
+        System.out.print(1);
+        return;
+    }
+    if (n % 2 == 0) {
+        writeSquares(n - 1);
+        System.out.print(", " + n * n);
+    }else {
+        
+    System.out.print(n * n + ", ");
+    writeSquares(n - 2);
+    System.out.print(", " + (n - 1) * (n - 1));
+    }
+}
