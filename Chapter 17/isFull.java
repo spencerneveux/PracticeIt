704f6839f64dfebd4564831686f13d56f3e42212
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-09 13:06:42 -0700

    Create isFull.java

---
+++ Chapter 17/isFull.java
@@ -0,0 +1,16 @@
+public boolean isFull() {
+    return isFull(overallRoot);
+}
+
+private boolean isFull(IntTreeNode root) {
+    if (root == null) 
+        return true;
+    
+    if (root.left == null && root.right != null) 
+        return false;
+    
+    if (root.right == null && root.left != null) 
+        return false;
+    
+    return isFull(root.left) && isFull(root.right);
+}
