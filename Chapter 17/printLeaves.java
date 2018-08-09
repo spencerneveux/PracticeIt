fac248bdad8d77d1b9cdb758eae9eb185640fdcf
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-09 13:06:22 -0700

    Create printLeaves.java

---
+++ Chapter 17/printLeaves.java
@@ -0,0 +1,22 @@
+public void printLeaves() {
+    if (overallRoot == null) {
+        System.out.print("no leaves");
+    }else {
+         System.out.print("leaves: ");
+    }
+
+    printLeaves(overallRoot);
+}
+
+private void printLeaves(IntTreeNode root) {
+    if (root == null) {
+        return;
+    }
+    
+    if (root.left == null && root.right == null) {
+        System.out.print(root.data + " ");
+        return;
+    }
+    printLeaves(root.right);
+    printLeaves(root.left);
+}
