34133ed6115005ed8637f80ddc04feca1fdef91b
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-09 12:44:15 -0700

    Update range.java

--- Chapter 6/range.java
+++ Chapter 6/range.java
@@ -1,1 +1,13 @@
-
+public static int range(int[] array) {
+    int min = array[0];
+    int max = array[array.length - 1];
+    for (int i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }else if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    int difference = (max - min) + 1;
+    return difference;
+}
