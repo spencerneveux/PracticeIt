3499159bd8a08a0c096d2c7fe9260a6d2772cd16
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-09 12:58:26 -0700

    Create quadrantPoint.java

---
+++ Chapter 8/quadrantPoint.java
@@ -0,0 +1,13 @@
+public int quadrant() {
+    if (x > 0 && y > 0) {
+        return 1;
+    }else if (x < 0 && y > 0) {
+        return 2;
+    }else if (x < 0 && y < 0) {
+        return 3;
+    }else if (x > 0 && y < 0){
+        return 4;
+    }else {
+        return 0;
+    }
+}
