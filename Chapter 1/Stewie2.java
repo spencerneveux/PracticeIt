0e64fc261b6f6620e6656d55c0bd1c8e3f8ee489
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-10 12:18:54 -0700

    Create Stewie2.java

---
+++ Chapter 1/Stewie2.java
@@ -0,0 +1,13 @@
+public class Stewie2 {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        for (int i = 0; i < 5; i++) {
+            printStewie();
+        }
+    }
+    
+    public static void printStewie() {
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
