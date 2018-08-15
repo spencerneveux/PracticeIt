a735490c5bdb888c44485ff379bc851472764e79
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-08-15 13:50:58 -0700

    Create classRectangle.java

---
+++ Chapter 8/classRectangle.java
@@ -0,0 +1,34 @@
+public class Rectangle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    
+    public Rectangle(int x, int y, int width, int height) {
+        if (width < 0 || height < 0) {throw new IllegalArgumentException();}
+        this.x = x;
+        this.y = y; 
+        this.width = width;
+        this.height = height;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public String toString() {
+        return "Rectangle[x=" + x + ",y=" + y + ",width=" + width + ",height=" + height + "]";
+    }
+}
