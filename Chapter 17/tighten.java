d8fbc41ee27887c45aac6308f5057c4e357ab19d
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-09-01 13:57:55 -0700

    Create tighten.java

---
+++ Chapter 17/tighten.java
@@ -0,0 +1,17 @@
+public void tighten() {
+    overallRoot = tighten(overallRoot);
+}
+
+private IntTreeNode tighten(IntTreeNode root) {
+    if (root == null) {
+        return null;
+    }else if (root.left == null && root.right != null) {
+        return tighten(root.right);
+    }else if (root.right == null && root.left != null) {
+        return tighten(root.left);
+    }else {
+        root.left = tighten(root.left);
+        root.right = tighten(root.right);
+        return root;
+    }
+}
