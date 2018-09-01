db1ee832b25628210ea2d562ed086b710a98e8e1
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-09-01 11:15:49 -0700

    Update printLevel.java
    Better

--- Chapter 17/printLevel.java
+++ Chapter 17/printLevel.java
@@ -1,22 +1,21 @@
-public void printLevel(int n) {
-    if (n < 1) {
-        throw new IllegalArgumentException();
-    }
-    
-    printLevel(overallRoot, n);
+public void printLevel(int n) {
+    if (n < 1) {
+        throw new IllegalArgumentException();
+    }else {
+        printLevel(overallRoot, n, 1);
+    }
 }
 
-private void printLevel(IntTreeNode root, int level){
-    if (root == null) {
-        return;
-    }
-    
-    if (level == 1) {
-        System.out.println(root.data);
-        return;
-    }
-    
-    printLevel(root.left, level - 1);
-    printLevel(root.right, level - 1);
-    
+private void printLevel(IntTreeNode root, int level, int depth) {
+    if (root == null) {
+        return;
+    }
+    
+    if (depth == level) {
+        System.out.println(root.data);
+        return;
+    }
+    
+    printLevel(root.left, level, depth + 1);
+    printLevel(root.right, level, depth + 1);
 }
