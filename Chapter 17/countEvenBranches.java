2159f5f8be7ef528ec1ea5e03f2b93d778ad3dcc
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-09-01 11:00:42 -0700

    Update countEvenBranches.java

--- Chapter 17/countEvenBranches.java
+++ Chapter 17/countEvenBranches.java
@@ -1,15 +1,17 @@
-public int countEvenBranches() {
-    return countEvenBranches(overallRoot);
+public int countEvenBranches() {
+    return countEvenBranches(overallRoot);
 }
 
-private int countEvenBranches(IntTreeNode root) {
-    if (root == null) {
-        return 0;
-    }else if (root.left != null || root.right != null) {
-        if (root.data % 2 == 0) {
-            return 1 + countEvenBranches(root.left) + countEvenBranches(root.right);
-        }
-        return countEvenBranches(root.left) + countEvenBranches(root.right);
-    }
-    return countEvenBranches(root.left) + countEvenBranches(root.right);
+private int countEvenBranches(IntTreeNode root) {
+    if (root == null) {
+        return 0;
+    }
+    
+    int count = 0;
+    if (root.left != null || root.right != null) {
+        if (root.data % 2 == 0) 
+            count++;
+    }
+    
+    return count + countEvenBranches(root.left) + countEvenBranches(root.right);
 }
