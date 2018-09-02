87ed1da9e857512b1edbb95e73a2d06311988a68
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2018-09-02 09:00:28 -0700

    Update doubleList.java
    Better

--- Chapter 10/doubleList.java
+++ Chapter 10/doubleList.java
@@ -1,14 +1,7 @@
 public void doubleList(ArrayList<String> list) {
-    List<String> newList = new ArrayList<>();
-    
-    for (String words : list) {
-        newList.add(words);
-        newList.add(words);
+    int size = list.size();
+    for (int i = 0; i < size; i++) {
+        String word = list.get(2 * i);
+        list.add(2 * i + 1, word);
     }
-    
-    list.clear();
-    
-    for (String words : newList) {
-        list.add(words);
-    }   
 }
